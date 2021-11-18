b473b51366ca76c7b04963ebd8462b1c7603a1c2
heojieun <gjekdmlwl@naver.com>
2021-11-18 17:13:04 +0900

    프린트 예제

---
+++ java13/src/Printer.java
@@ -0,0 +1,22 @@
+public class Printer
+{
+	static void println(int result)
+	{
+		System.out.println(result);
+	}
+	
+	static void println(boolean result)
+	{
+		System.out.println(result);
+	}
+	
+	static void println(double result)
+	{
+		System.out.println(result);
+	}
+	
+	static void println(String result)
+	{
+		System.out.println(result);
+	}
+}
---
+++ java13/src/PrinterExample.java
@@ -0,0 +1,18 @@
+public class PrinterExample
+{
+	public static void main(String[] args)
+	{
+		Printer printer = new Printer();
+		printer.println(10);
+		printer.println(true);
+		printer.println(5.7);
+		printer.println("ȫ�浿");
+		
+		//Printer printer = new Printer();
+		Printer.println(10);
+		Printer.println(true);
+		Printer.println(5.7);
+		Printer.println("ȫ�浿");
+	}
+
+}
