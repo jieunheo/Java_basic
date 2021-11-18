c6b4e423efae77de636646c08d512e05eabb96c1
heojieun <gjekdmlwl@naver.com>
2021-11-18 16:41:07 +0900

    continue 구문

---
+++ java03/src/Example09.java
@@ -0,0 +1,14 @@
+public class Example09 
+{
+	public static void main(String[] args) 
+	{
+		for ( int i = 0; i <= 100; i++ )
+		{
+			if ( ( i % 2 ) == 1 )
+			{
+				continue;
+			}
+			System.out.println( i );
+		}
+	}
+}
