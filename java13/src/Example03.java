8b28d845bda0570a89bf8ae81721899668d7480b
heojieun <gjekdmlwl@naver.com>
2021-11-18 17:14:22 +0900

    아이티와 비밀번호로 로그인 로그아웃

---
+++ java13/src/Example03.java
@@ -0,0 +1,20 @@
+public class Example03
+{
+	String name;	 //이름
+	String id;		 //아이디
+	String password; //비밀번호
+	int    age;		 //나이
+	
+	//생성자
+	public Example03(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+	
+	
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
