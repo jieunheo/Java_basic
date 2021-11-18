dc3e114acbd1c5efc80eb09dbcf09c3274b78b89
heojieun <gjekdmlwl@naver.com>
2021-11-18 16:40:42 +0900

    break 구문

---
+++ java03/src/Example08.java
@@ -0,0 +1,16 @@
+public class Example08 
+{
+	public static void main(String[] args) 
+	{
+		//break 구문 연습
+		for ( int i = 100 ; i >= 0 ; i-- )
+		{
+			System.out.println( i );
+			
+			if ( i == 90 )
+			{
+				break;
+			}
+		}
+	}
+}
