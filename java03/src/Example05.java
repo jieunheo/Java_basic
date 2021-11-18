efe26970e2d13f43636373c2dace95fd48895d13
heojieun <gjekdmlwl@naver.com>
2021-11-18 16:39:48 +0900

    반복문 do-while

---
+++ java03/src/Example05.java
@@ -0,0 +1,13 @@
+public class Example05 
+{
+	public static void main(String[] args) 
+	{
+		int i = 0;
+		
+		do
+		{
+			System.out.println( "this is do " + i );
+			i++;
+		} while ( i <= 10 );
+	}
+}
